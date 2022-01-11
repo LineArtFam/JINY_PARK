7ed6dd86f7aa1063d0c4debe05b2bcd6e9b71ca6
Park Jinyong <79570234+Jinypak@users.noreply.github.com>
2022-01-11 11:58:02 +0900

    update navbar styling

--- board/src/components/Navbar.jsx
+++ board/src/components/Navbar.jsx
@@ -2,13 +2,47 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Navbar() {
-  return <NavContainer></NavContainer>;
+  return (
+    <NavContainer>
+      <div className="navLeft">
+        <img src="http://3.34.175.3/assets/img/jlucy-logo.png" alt="" />
+      </div>
+      <div className="navCenter">JIN.</div>
+      <div className="navRight">
+        <ul className="menuList">
+          <li className="menuListItem">Home</li>
+          <li className="menuListItem">board</li>
+          <li className="menuListItem">Home</li>
+          <li className="menuListItem">Home</li>
+        </ul>
+      </div>
+    </NavContainer>
+  );
 }
 
 const NavContainer = styled.div`
-    width: 100%;
-    height: 50px;
-    position: sticky;
-    top: 0;
-    background: #ccc;
+  width: 100%;
+  height: 50px;
+  position: sticky;
+  top: 0;
+  background: #ccc;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .navRight {
+    height: 100vh;
+    .menuList {
+      background: #ccc;
+      display: flex;
+      height: 100%;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      .menuListItem {
+        text-decoration: none;
+        list-style: none;
+      }
+    }
+  }
 `;
